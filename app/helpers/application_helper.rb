01c2da5b1d419892ce5a12c2088207bcb473e690
Long Truong <ngoclong31120@gmail.com>
2023-08-17 15:41:17 +0700

    adjust categories const into topnav and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  def article_categories
-    [
-      { name: 'Economy', uri: 'economy' },
-      { name: 'Politics & Laws', uri: 'politics-and-laws' },
-      { name: 'Society', uri: 'society' },
-      { name: 'Sports', uri: 'sports' },
-      { name: 'Tech', uri: 'tech' },
-      { name: 'Health', uri: 'health' }
-    ]
-  end
 end
