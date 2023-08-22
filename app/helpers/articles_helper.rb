92f9efcc6c66455ca9ebba6cd6ae2ef1e60d5f27
Long Truong <ngoclong31120@gmail.com>
2023-08-22 17:05:45 +0700

    add href and hover effect

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -10,4 +10,20 @@ module ArticlesHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def format_date(date)
+    day = date.day
+    suffix = case day
+             when 1, 21, 31
+               '<sup>st</sup>'
+             when 2, 22
+               '<sup>nd</sup>'
+             when 3, 23
+               '<sup>rd</sup>'
+             else
+               '<sup>th</sup>'
+             end
+  
+    "#{date.strftime('%b')} #{day}#{suffix}, #{date.year}".html_safe
+  end
 end
