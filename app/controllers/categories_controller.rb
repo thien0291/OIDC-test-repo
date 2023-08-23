fb4391506d2a0b26fae3a65b27f6b021b8372a6f
Thien Tran <webmaster3t@gmail.com>
2023-08-23 12:13:31 +0700

    update categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    category = CATEGORIES.find { |item| item[:id] == params[:id] }
-    @category_name = category[:name]
-    @category_id = category[:id]
-
-    @articles_by_category = Article.tagged_with(@category_name.downcase).paginate(page: params[:page], per_page: 5)
+    @category_name = params[:name]
+    @articles_by_category = Article.tagged_with(@category_name.downcase, on: :categories).paginate(page: params[:page], per_page: 5)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,17 +32,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_031212) do
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
-  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.uuid "record_id", null: false
-    t.uuid "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -54,8 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_031212) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "blob_id", null: false
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -276,8 +276,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_031212) do
     t.index ["name"], name: "motor_tags_name_unique_index", unique: true
   end
 
-  create_table "taggings", force: :cascade do |t|
-    t.bigint "tag_id"
+  create_table "taggings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "tag_id"
     t.string "taggable_type"
     t.uuid "taggable_id"
     t.string "tagger_type"
@@ -299,7 +299,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_031212) do
     t.index ["tenant"], name: "index_taggings_on_tenant"
   end
 
-  create_table "tags", force: :cascade do |t|
+  create_table "tags", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
