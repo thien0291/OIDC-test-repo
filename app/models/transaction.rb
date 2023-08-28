1d4e667f60be60d07476f8be7eeb5b3bc47b2587
Long Truong <ngoclong31120@gmail.com>
2023-08-28 10:13:53 +0700

    sidebar UI

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -10,4 +10,21 @@ class Transaction < ApplicationRecord
                  disputed: "disputed",
                  canceled: "canceled",
                  refunded: "refunded" }
+
+  scope :sum_amount_current_month, -> {
+    current_month = Time.current.month
+    current_year = Time.current.year
+
+    where("EXTRACT(MONTH FROM created_at) = ? AND EXTRACT(YEAR FROM created_at) = ?", current_month, current_year)
+      .sum(:amount)
+  }
+
+  scope :latest_completed_access_pass_package_name, -> {
+    joins("JOIN access_passes AS ap ON transactions.related_object_id = ap.id")
+      .where("transactions.status = 'completed'")
+      .order("transactions.created_at DESC")
+      .limit(1)
+      .pluck("ap.package_name")
+      .first
+  }
 end
